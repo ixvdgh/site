bea37577df63b3505173b24d189185c20f2b1b0f
faulty <didier@faulty.nl>
2023-01-19 11:34:04 +0100

    add: liberapay

--- dev.sh
+++ dev.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 server_nw() {
-  python3 -m http.server 8080 &
+  cd dist
+  python3 -m http.server 8000 &
   SERVER_PID=$!
   trap "kill $SERVER_PID" EXIT
 }
 
 watch() {
-  nodemon -e hbs,js,css -w src -x "npm run compile dev"
+  nodemon -e hbs,js,css -w ../src -x "npm run compile dev"
 }
 
 server_nw && watch
